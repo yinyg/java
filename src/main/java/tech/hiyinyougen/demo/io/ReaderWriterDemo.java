44a7a2514f2103bf94e0e043f2854eb770fe6777
殷有根 <yougen.yin@five-star.cn>
2019-07-02 17:53:53 +0800

    demo

--- src/main/java/tech/hiyinyougen/demo/io/ReaderWriterDemo.java
+++ src/main/java/tech/hiyinyougen/demo/io/ReaderWriterDemo.java
@@ -1,8 +1,65 @@
 package tech.hiyinyougen.demo.io;
 
+import java.io.*;
+import java.util.Random;
+
 /**
  * @author yinyg
  * @date 2019/7/2
+ * 字符流demo
  */
 public class ReaderWriterDemo {
+    public static void main(String[] args) {
+        String source = "/Users/yinyougen/projects/demo/java/src/main/resources/static/io.txt";
+        String target = "/Users/yinyougen/projects/demo/java/src/main/resources/static/io_"
+                + new Random().nextInt(9999)
+                + ".txt";
+
+        TxtFileUtils.copy(source, target);
+    }
+}
+
+class TxtFileUtils {
+    public static void copy(String source, String target) {
+//        File sourceFile = new File(source);
+//        File targetFile = new File(target);
+
+        FileReader fileReader = null;
+        FileWriter fileWriter = null;
+
+        try {
+//            fileReader = new FileReader(sourceFile);
+//            fileWriter = new FileWriter(targetFile);
+
+            fileReader = new FileReader(source);
+            fileWriter = new FileWriter(target);
+
+            int flag = -1;
+
+            while (-1 != (flag = fileReader.read())) {
+                fileWriter.write(flag);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != fileReader) {
+                try {
+                    fileReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (null != fileWriter) {
+                try {
+                    fileWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        System.out.println("文本文件复制成功");
+    }
 }
--- src/main/java/tech/hiyinyougen/demo/io/StreamDemo.java
+++ src/main/java/tech/hiyinyougen/demo/io/StreamDemo.java
@@ -1,8 +1,71 @@
 package tech.hiyinyougen.demo.io;
 
+import java.io.*;
+import java.util.Random;
+
 /**
  * @author yinyg
  * @date 2019/7/2
+ * 字节流demo
  */
 public class StreamDemo {
+    public static void main(String[] args) {
+        String source = "/Users/yinyougen/projects/demo/java/src/main/resources/static/io.txt";
+        String target = "/Users/yinyougen/projects/demo/java/src/main/resources/static/io_"
+                + new Random().nextInt(9999)
+                + ".txt";
+
+        ImgUtils.copy(source, target);
+    }
+}
+
+class ImgUtils {
+    /**
+     * copy图片
+     * @param source 源文件路径
+     * @param target 目标文件路径
+     */
+    public static void copy(String source, String target) {
+//        File sourceFile = new File(source);
+//        File targetFile = new File(target);
+
+        InputStream in = null;
+        OutputStream out = null;
+
+        try {
+//            in = new FileInputStream(sourceFile);
+//            out = new FileOutputStream(targetFile);
+
+            in = new FileInputStream(source);
+            out = new FileOutputStream(target);
+
+            byte[] bytes = new byte[1024];
+            int len = -1;
+
+            while (-1 != (len = in.read(bytes))) {
+                out.write(bytes, 0, len);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != in) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (null != out) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        System.out.println("文件复制成功");
+    }
 }
