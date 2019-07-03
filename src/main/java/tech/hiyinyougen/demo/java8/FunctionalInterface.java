fbb81a99b201f5870dca969263f013516dde21af
殷有根 <yougen.yin@five-star.cn>
2019-07-03 16:36:20 +0800

    函数式接口

---
+++ src/main/java/tech/hiyinyougen/demo/java8/FunctionalInterface.java
@@ -0,0 +1,13 @@
+package tech.hiyinyougen.demo.java8;
+
+import java.util.List;
+
+/**
+ * @Author yinyg
+ * @CreateTime 2019/7/3 16:00
+ * @Description
+ */
+@java.lang.FunctionalInterface
+public interface FunctionalInterface<T> {
+    void handle(int a, int b);
+}
---
+++ src/main/java/tech/hiyinyougen/demo/java8/FunctionalInterfaceDemo.java
@@ -0,0 +1,14 @@
+package tech.hiyinyougen.demo.java8;
+
+/**
+ * @Author yinyg
+ * @CreateTime 2019/7/3 16:07
+ * @Description
+ */
+public class FunctionalInterfaceDemo {
+    public static void main(String[] args) {
+        FunctionalInterface f = (int a, int b) -> System.out.println(a + " + " + b + " = " + (a + b));
+
+        f.handle(1, 2);
+    }
+}
