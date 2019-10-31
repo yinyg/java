d45e3ea5fa0ada7a48afd06f0961ac9feb730b53
殷有根 <yougen.yin@five-star.cn>
2019-10-31 18:40:00 +0800

    demo

--- src/main/java/tech/hiyinyougen/java/JavaApplication.java
+++ src/main/java/tech/hiyinyougen/java/JavaApplication.java
@@ -2,11 +2,18 @@ package tech.hiyinyougen.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import tech.hiyinyougen.java.dto.UserSaveDTO;
 
 @SpringBootApplication
 public class JavaApplication {
 
 	public static void main(String[] args) {
+		ApplicationContext context = new FileSystemXmlApplicationContext("classpath:test.xml");
+		UserSaveDTO user = (UserSaveDTO)context.getBean("user");
+		System.out.println(user);
+
 		SpringApplication.run(JavaApplication.class, args);
 	}
 
